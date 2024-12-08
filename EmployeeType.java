ba96e2319c73910d2683a9d39ab1551b8ccfdc14
DESKTOP-SM6AS48\arifr <arifrs789@gmail.com>
2024-12-08 20:03:11 +0700

    employeetree

---
+++ EmployeeType.java
@@ -0,0 +1,16 @@
+
+public class EmployeeType {
+        abstract int payAmount(Employee emp);
+}
+class Salesman {
+    int payAmount(Employee emp) {
+        return emp.getMonthlySalary() +emp.getCommission();
+    }
+
+}
+class Manager {
+    int payAmount(Employee emp) {
+        return emp.getMonthlySalary() + emp.getBonus();
+    }
+
+}
