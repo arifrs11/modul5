6060c7c4458c3bbc32421cd4056c883caa2f2159
DESKTOP-SM6AS48\arifr <arifrs789@gmail.com>
2024-12-08 20:40:53 +0700

    tugas

---
+++ Tugas1.java
@@ -0,0 +1,11 @@
+public class Tugas1 {
+    public static void main(String[] args) {
+        try {
+            System.out.println(3/0);
+        } catch(Exception e) {
+            
+        }
+
+    }
+
+}
