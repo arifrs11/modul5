85b96cfef6e2ff115067b0f3dd2c23e2a4b0f1d6
DESKTOP-SM6AS48\arifr <arifrs789@gmail.com>
2024-12-08 20:06:26 +0700

    class point after

--- Point.java
+++ Point.java
@@ -1,5 +1,16 @@
-public class Point { 
-public double x;
-public double y;
+public interface Point {
 
-}
+    double getX();
+    
+    double getY();
+    
+    void setCartesian(double x, double y);
+    
+    double getR();
+    
+    double getTheta();
+    
+    void setPolar(double r, double theta);
+    
+    }
+    
